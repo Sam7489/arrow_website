765d1915aedee5c1dd45c7af750f4f072888800f
Sam7489 <shivku7489@gmail.com>
2025-05-23 22:07:48 +0530

    adding procfile

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from waitress import serve
 from app import app  # Make sure this matches your Flask file and object
 
-serve(app, host='0.0.0.0', port=80)
+if __name__ == '__main__':
+    app.run()
